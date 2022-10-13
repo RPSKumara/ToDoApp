3ad9845a6b22b4019f1009aa0e4d41a88dd3f39c
Roshan Prabath Sisira Kumara <91238112+RPSKumara@users.noreply.github.com>
2022-10-13 17:31:38 +0530

    created todo app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import File from "./components/File";
 import ToDo from "./components/ToDo";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div>
+      <Typography
+        variant="h5"
+        sx={{
+          color: "red",
+          p: "20px",
+          textAlign: "center",
+          textShadow: "1px 1px 2px white, 0 0 5px, 0 0 5px white",
+          fontSize: "80px",
+        }}
+      >
+        Todo App
+      </Typography>
       {/* <File /> */}
       <ToDo />
     </div>
